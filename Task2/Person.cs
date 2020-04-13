c0e3efc15c85490a518f1577a3faf43271f7e4e9
Valen-Tine <62900178+Valen-Tine@users.noreply.github.com>
2020-04-13 14:35:30 +0300

    Names

---
+++ Task2/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class Person
+    {
+        string first_name = "with unknown last name";
+        string last_name = "with unknown first name";
+        int min_year = 5000;
+
+        
+        public Dictionary<int, string> Changes = new Dictionary<int, string> { };
+
+        public void ChangeFirstName(int year, string first_name)
+        {
+            if (min_year > year)
+                min_year = year;
+            this.first_name = first_name; 
+            Changes.Add(year, first_name + " " + last_name);
+        }
+
+        public void ChangeLastName(int year, string last_name)
+        {
+            if (min_year > year)
+                min_year = year;
+
+            this.last_name = last_name;
+            Changes.Add(year, first_name + " " + last_name);
+        }
+
+        public void GetFullName(int year)
+        {
+            int c = 0;
+            if (Changes.ContainsKey(year))
+                Console.WriteLine($"В {year} году имя {Changes[year]}.");
+            else if(!Changes.ContainsKey(year) && year > min_year)
+            {
+                Changes[year] = Changes[year - 1];
+                Console.WriteLine($"В {year} году имя {Changes[year]}.");
+            }
+
+            else if(year < min_year)
+                Console.WriteLine("Incognito");
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.ChangeFirstName(1999, "Helen");
+            person.ChangeLastName(2001, "Kot");
+            person.ChangeFirstName(2004, "Ruby");
+            person.ChangeLastName(2005, "Krets");
+
+            for (int i = 1996; i < 2006; i++)
+            {
+                person.GetFullName(i);
+            }
+        }
+    }
+}
